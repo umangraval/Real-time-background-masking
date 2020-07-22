4a5e17ffa9cdda2e7e8d2f7475e67c79bf030c11
umangraval <umangraval2000@gmail.com>
2020-07-22 21:34:36 +0530

    commented redirected stream

--- app/main.py
+++ app/main.py
@@ -19,11 +19,11 @@ def index():
     """Video streaming home page."""
     return render_template('index.html')
 
-def gen(camera):
-    while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+# def gen(camera):
+#     while True:
+#         frame = camera.get_frame()
+#         yield (b'--frame\r\n'
+#                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 	
 @app.route('/upload', methods=['POST', 'GET'])
@@ -45,83 +45,83 @@ def upload_file():
             return redirect('/')
 
 
-# def resize(dst,img):
-#     width = img.shape[1]
-#     height = img.shape[0]
-#     dim = (width, height)
-#     resized = cv2.resize(dst, dim, interpolation = cv2.INTER_AREA)
-#     return resized
+def resize(dst,img):
+    width = img.shape[1]
+    height = img.shape[0]
+    dim = (width, height)
+    resized = cv2.resize(dst, dim, interpolation = cv2.INTER_AREA)
+    return resized
 
         
-# def gen(key):
-#     cap = cv2.VideoCapture('time.mp4')
+def gen(key):
+    cap = cv2.VideoCapture('time.mp4')
     
-#     # Read until video is completed
-#     while(cap.isOpened()):
-#       # Capture frame-by-frame
-#         ret, img = cap.read()
-#         if ret == True:
-#             img = cv2.resize(img, (640,480), fx=0.5, fy=0.5) 
-#             frame = cv2.imencode('.jpg', img)[1].tobytes()
-#             yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-#             time.sleep(0.1)
-#         else: 
-#             break
+    # Read until video is completed
+    while(cap.isOpened()):
+      # Capture frame-by-frame
+        ret, img = cap.read()
+        if ret == True:
+            img = cv2.resize(img, (640,480), fx=0.5, fy=0.5) 
+            frame = cv2.imencode('.jpg', img)[1].tobytes()
+            yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            time.sleep(0.1)
+        else: 
+            break
     
-#     video = cv2.VideoCapture(0)    
-#     # time.sleep(10)
-#     success, ref_img = video.read()
-#     flag = 0
+    video = cv2.VideoCapture(0)    
+    # time.sleep(10)
+    success, ref_img = video.read()
+    flag = 0
     
-#     while(1):
-#         success, img = video.read()
-#         bg = cv2.imread('./static/background.jpg')
-#         bg = resize(bg, ref_img)
-#         if flag==0:
-#             ref_img = img
-#         # create a mask
-#         diff1 = cv2.subtract(img, ref_img)
-#         diff2 = cv2.subtract(ref_img,img)
-#         diff = diff1+diff2
-#         diff[abs(diff)<20.0]=0
-#         gray = cv2.cvtColor(diff.astype(np.uint8), cv2.COLOR_BGR2GRAY)
-#         gray[np.abs(gray)< 10] = 0
-#         fgmask=gray.astype(np.uint8)
-#         fgmask[fgmask>5]=255
-#         fgmask = cv2.medianBlur(fgmask,5)
+    while(1):
+        success, img = video.read()
+        bg = cv2.imread('./static/background.jpg')
+        bg = resize(bg, ref_img)
+        if flag==0:
+            ref_img = img
+        # create a mask
+        diff1 = cv2.subtract(img, ref_img)
+        diff2 = cv2.subtract(ref_img,img)
+        diff = diff1+diff2
+        diff[abs(diff)<20.0]=0
+        gray = cv2.cvtColor(diff.astype(np.uint8), cv2.COLOR_BGR2GRAY)
+        gray[np.abs(gray)< 10] = 0
+        fgmask=gray.astype(np.uint8)
+        fgmask[fgmask>5]=255
+        fgmask = cv2.medianBlur(fgmask,5)
         
-#         #invert the mask
-#         fgmask_inv = cv2.bitwise_not(fgmask)
-#         fgmask_inv = cv2.medianBlur(fgmask_inv,5)
+        #invert the mask
+        fgmask_inv = cv2.bitwise_not(fgmask)
+        fgmask_inv = cv2.medianBlur(fgmask_inv,5)
 
-#         #use the mask to extract the relevent parts from FG and BG
-#         fgimg = cv2.bitwise_and(img,img,mask=fgmask)
-#         bgimg = cv2.bitwise_and(bg,bg,mask=fgmask_inv)
-#         #combine both bg and fg images
-#         dst = cv2.add(bgimg, fgimg)
-#         # cv2.imshow('Backgroung removal', dst)
-#         frame = cv2.imencode('.jpg', dst)[1].tobytes()
-#         yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-#         # key = cv2.waitKey(10)
-#         if ord('q') == key:
-#             break
-#         elif ord('d') ==key:
-#             flag = 1
-#             # print('background captured')
-#         elif ord('r') == key:
-#             flag = 0
-            # print('ready to capture new bg')
+        #use the mask to extract the relevent parts from FG and BG
+        fgimg = cv2.bitwise_and(img,img,mask=fgmask)
+        bgimg = cv2.bitwise_and(bg,bg,mask=fgmask_inv)
+        #combine both bg and fg images
+        dst = cv2.add(bgimg, fgimg)
+        # cv2.imshow('Backgroung removal', dst)
+        frame = cv2.imencode('.jpg', dst)[1].tobytes()
+        yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        # key = cv2.waitKey(10)
+        if ord('q') == key:
+            break
+        elif ord('d') ==key:
+            flag = 1
+            # print('background captured')
+        elif ord('r') == key:
+            flag = 0
+            print('ready to capture new bg')
 
-# @app.route('/video_feed')
-# def video_feed():
-#     """Video streaming route. Put this in the src attribute of an img tag."""
-#     return Response(gen(100),
-#                     mimetype='multipart/x-mixed-replace; boundary=frame')
-
-@app.route('/video_fed')
-def video_fed():
-    return Response(gen(VideoCamera()),
+@app.route('/video_feed')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(100),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
+
+# @app.route('/video_fed')
+# def video_fed():
+#     return Response(gen(VideoCamera()),
+#                     mimetype='multipart/x-mixed-replace; boundary=frame')
    
 
 if __name__ == '__main__':
